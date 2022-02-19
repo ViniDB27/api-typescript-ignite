15b29591f676c41cf8d771eb8962adc8e4500334
vinicio brejisnki <viniciosdb@gmail.com>
2022-02-19 17:02:30 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const categoriesRotues = Router();
+
+const categories = [];
+
+categoriesRotues.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const category = {
+    id: uuidv4(),
+    name,
+    description,
+  };
+
+  categories.push(category);
+
+  return response.status(201).json(category);
+});
+
+export { categoriesRotues };
