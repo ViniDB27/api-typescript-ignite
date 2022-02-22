2a22779d47705acaca50609104996049bbb82d70
vinicio brejisnki <viniciosdb@gmail.com>
2022-02-22 16:30:14 -0300

    feat: upload file with multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCatergoriesContoller } from "../modules/cars/useCases/listCategories";
 
 const categoriesRotues = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRotues.get("/", listCatergoriesContoller.handle);
 categoriesRotues.post("/", createCategoryController.handle);
+
+categoriesRotues.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+
+  console.log(file);
+
+  return response.send();
+});
 
 export { categoriesRotues };
