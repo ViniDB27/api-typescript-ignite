b2bf08d22a74ea741c33f26f4cc624d59fcfa879
vinicio brejisnki <viniciosdb@gmail.com>
2022-02-22 20:00:41 -0300

    read file csv wih stream and csv parse

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,8 +1,9 @@
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./importCategoryUseCase";
 
-
 const importCategoryUseCase = new ImportCategoryUseCase();
-const importCategoryController = new ImportCategoryController(importCategoryUseCase);
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase
+);
 
 export { importCategoryController };
