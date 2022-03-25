825c3f2d7a7033c4fb5ec2011d061bdf294d7e06
vinicio brejinski <viniciosdb@gmail.com>
2022-03-25 14:24:10 -0300

    middleware by token

--- src/middleware/ensureAuthenticated.ts
+++ src/middleware/ensureAuthenticated.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
+import { AppError } from "../errors/AppErrores";
+import { UserRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
 export async function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -8,13 +15,24 @@ export async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization;
 
-  if (authHeader) throw new Error("oken missing");
+  if (authHeader) throw new AppError("Token missing", 401);
 
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = verify(token, "secretOrPrivateKeyExample");
+    const { sub: user_id } = verify(
+      token,
+      "secretOrPrivateKeyExample"
+    ) as IPayload;
+
+    const usersRepository = new UserRepository();
+
+    const user = usersRepository.findById(user_id);
+
+    if (!user) throw new AppError("User not exist", 401);
+
+    next();
   } catch {
-    throw new Error("Invalid Token");
+    throw new AppError("Invalid Token", 401);
   }
 }
